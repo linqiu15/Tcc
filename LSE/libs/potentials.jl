1eee5bcd8d68eb0462b41cfd73008cceffafe532
ql <qiulin@ihep.ac.cn>
2022-12-08 16:48:56 +0800

    version:a1 deform+on-shell saturation

--- LSE/libs/potentials.jl
+++ LSE/libs/potentials.jl
@@ -1,5 +1,5 @@
 # ----------------------------------------------------------DD*----------------------------------------------------#
-function potential_DnDsc(E, p, q; I=0, a=0.0, a1=0.0, isπ=true)
+function potential_DnDsc(E, p, q, pon, qon; I=0, a=0.0, a1=0.0, isπ=true)
     q01, q02 = q0f(E, mDn, mDsc, mDn, mDsc), q0f(E, mDn, mDsc, mDsc, mDn)
     res = zero(ComplexF64)
     res += -β^2 * gV^2 * (1 - q01^2 / mρn^2) / sqrt(16) * pwa_central(p, q, mρn^2 - q01^2)
@@ -8,12 +8,12 @@ function potential_DnDsc(E, p, q; I=0, a=0.0, a1=0.0, isπ=true)
     if isπ == true
         res += -4 * g^2 / fπ^2 / sqrt(16) * pwa_tensor_ss_deform(p, q, mπc^2 - q02^2; a=a1) * (I - 1 / 2) * 2
     end
-    res += ((2 * sqrt(2) * gV * gλ)^2 / sqrt(16) * pwa_tensor_ss(p, q, mρc^2 - q02^2; a=a) * (I - 1 / 2) * 2)
+    res += ((2 * sqrt(2) * gV * gλ)^2 / sqrt(16) * pwa_vvv_ss(pon, qon, mρc^2 - q02^2; a=a) * (I - 1 / 2) * 2)
     res += -β^2 * gV^2 * (1 - q01^2 / mJψ^2) / sqrt(16) * pwa_central(p, q, mJψ^2 - q01^2) * (-2)
     return res
 end
 
-function potential_DcDsn(E, p, q; I=0, a=0.0, a1=0.0, isπ=true)
+function potential_DcDsn(E, p, q, pon, qon; I=0, a=0.0, a1=0.0, isπ=true)
     q01, q02 = q0f(E, mDc, mDsn, mDc, mDsn), q0f(E, mDc, mDsn, mDsn, mDc)
     res = zero(ComplexF64)
 
@@ -23,12 +23,12 @@ function potential_DcDsn(E, p, q; I=0, a=0.0, a1=0.0, isπ=true)
     if isπ == true
         res += -4 * g^2 / fπ^2 / sqrt(16) * pwa_tensor_ss_deform(p, q, mπc^2 - q02^2; a=a1) * (I - 1 / 2) * 2
     end
-    res += ((2 * sqrt(2) * gV * gλ)^2 / sqrt(16) * pwa_tensor_ss(p, q, mρc^2 - q02^2; a=a) * (I - 1 / 2) * 2)
+    res += ((2 * sqrt(2) * gV * gλ)^2 / sqrt(16) * pwa_vvv_ss(pon, qon, mρc^2 - q02^2; a=a) * (I - 1 / 2) * 2)
     res += -β^2 * gV^2 * (1 - q01^2 / mJψ^2) / sqrt(16) * pwa_central(p, q, mJψ^2 - q01^2) * (-2)
     return res
 end
 
-function potential_DDscross(E, p, q; I=0, a=0.0, a1=0.0, isπ=true)
+function potential_DDscross(E, p, q, pon, qon; I=0, a=0.0, a1=0.0, isπ=true)
     q01, q02 = q0f(E, mDn, mDsc, mDc, mDsn), q0f(E, mDn, mDsc, mDsn, mDc)
     res = zero(ComplexF64)
 
@@ -36,82 +36,69 @@ function potential_DDscross(E, p, q; I=0, a=0.0, a1=0.0, isπ=true)
     if isπ == true
         res += 2 * g^2 / fπ^2 / sqrt(16) * pwa_tensor_ss_deform(p, q, mπn^2 - q02^2; a=a1) * (I - 1 / 2) * 2
     end
-    res += -2 * g^2 / (3fπ^2) / sqrt(16) * pwa_tensor_ss(p, q, mη^2 - q02^2; a=a) * (I - 1 / 2) * 2
-    res += (-(2 * gV * gλ)^2 / sqrt(16) * pwa_tensor_ss(p, q, mρn^2 - q02^2; a=a) * (I - 1 / 2) * 2)
-    res += ((2 * gV * gλ)^2 / sqrt(16) * pwa_tensor_ss(p, q, mω^2 - q02^2; a=a) * (I - 1 / 2) * 2)
+    res += -2 * g^2 / (3fπ^2) / sqrt(16) * pwa_tensor_ss(pon, qon, mη^2 - q02^2; a=a) * (I - 1 / 2) * 2
+    res += (-(2 * gV * gλ)^2 / sqrt(16) * pwa_vvv_ss(pon, qon, mρn^2 - q02^2; a=a) * (I - 1 / 2) * 2)
+    res += ((2 * gV * gλ)^2 / sqrt(16) * pwa_vvv_ss(pon, qon, mω^2 - q02^2; a=a) * (I - 1 / 2) * 2)
     #ηc,J/ψ
-    res += 2 * g^2 / fπ^2 / sqrt(16) * pwa_tensor_ss(p, q, mηc^2 - q02^2; a=a) * (-2) * (I - 1 / 2) * 2
-    res += (-(2 * gV * gλ)^2 / sqrt(16) * pwa_tensor_ss(p, q, mJψ^2 - q02^2; a=a) * (I - 1 / 2) * 2 * (-2))
+    res += 2 * g^2 / fπ^2 / sqrt(16) * pwa_tensor_ss(pon, qon, mηc^2 - q02^2; a=a) * (-2) * (I - 1 / 2) * 2
+    res += (-(2 * gV * gλ)^2 / sqrt(16) * pwa_vvv_ss(pon, qon, mJψ^2 - q02^2; a=a) * (I - 1 / 2) * 2 * (-2))
     return res
 end
 
 
-# ----------------------------------------------------------DD*bar----------------------------------------------------#
-function potential_Xn(E, p, q; C=1, a=0, a1=0.0, isπ=true)
+
+#----------------------------------------------------------DD*bar----------------------------------------------------#
+function potential_Xn(E, p, q, pon, qon; C=1, a=0, a1=0.0, isπ=true)
     q01, q02 = q0f(E, mDn, mDsn, mDn, mDsn), q0f(E, mDn, mDsn, mDsn, mDn)
     res = zero(ComplexF64)
 
     if isπ == true
-        res += (-C) * (√2 * g / fπ)^2 / 4 * pwa_tensor(p, q, mπn^2 - q02^2; cl=cl)
+        res += (-C) * (√2 * g / fπ)^2 / 4 * pwa_tensor_ss_deform(p, q, mπn^2 - q02^2; a=a1)
     end
-    res += (-C) * 1 / 3 * (√2 * g / fπ)^2 / 4 * pwa_tensor(p, q, mη^2 - q02^2; cl=cl)
-    res += -β^2 * gV^2 * (1 - q01^2 / mρn^2) / 4 * pwa_central(p, q, mρn^2 - q01^2; cl=cl)
-    res += ((-C) * (2 * gλ * gV)^2 / 4 * (pwa_tensor(p, q, mρn^2 - q02^2; cl=cl) + 2 * a / 3 * pwa_contact(p, q, mρn^2 - q02^2; cl=cl)
-                                          -
-                                          pwa_square(p, q, mρn^2 - q02^2; cl=cl)))
-    res += -β^2 * gV^2 * (1 - q01^2 / mω^2) / 4 * pwa_central(p, q, mω^2 - q01^2; cl=cl)
-    res += ((-C) * (2 * gλ * gV)^2 / 4 * (pwa_tensor(p, q, mω^2 - q02^2; cl=cl) + 2 * a / 3 * pwa_contact(p, q, mω^2 - q02^2; cl=cl)
-                                          -
-                                          pwa_square(p, q, mω^2 - q02^2; cl=cl)))
-    res += -4 * gs^2 / 4 * pwa_central(p, q, mσ^2 - q01^2; cl=cl)
+    res += (-C) * 1 / 3 * (√2 * g / fπ)^2 / 4 * pwa_tensor_ss(pon, qon, mη^2 - q02^2; a=a)
+    res += -β^2 * gV^2 * (1 - q01^2 / mρn^2) / 4 * pwa_central(p, q, mρn^2 - q01^2)
+    res += (-C) * (2 * gλ * gV)^2 / 4 * pwa_vvv_ss(pon, qon, mρn^2 - q02^2; a=a)
+    res += -β^2 * gV^2 * (1 - q01^2 / mω^2) / 4 * pwa_central(p, q, mω^2 - q01^2)
+    res += (-C) * (2 * gλ * gV)^2 / 4 * pwa_vvv_ss(pon, qon, mω^2 - q02^2; a=a)
+    res += -4 * gs^2 / 4 * pwa_central(p, q, mσ^2 - q01^2)
     #J/ψ
-    res += -β^2 * gV^2 * (1 - q01^2 / mJΨ^2) / 4 * pwa_central(p, q, mJΨ^2 - q01^2; cl=cl) * 2
-    res += ((-C) * (2 * gλ * gV)^2 / 4 * (pwa_tensor(p, q, mJΨ^2 - q02^2; cl=cl) + 2 * a / 3 * pwa_contact(p, q, mJΨ^2 - q02^2; cl=cl)
-                                          -
-                                          pwa_square(p, q, mJΨ^2 - q02^2; cl=cl)) * 2)
+    res += -β^2 * gV^2 * (1 - q01^2 / mJψ^2) / 4 * pwa_central(p, q, mJψ^2 - q01^2) * 2
+    res += (-C) * (2 * gλ * gV)^2 / 4 * pwa_vvv_ss(pon, qon, mJψ^2 - q02^2; a=a) * 2
     #ηc
-    res += (-C) * (√2 * g / fπ)^2 / 4 * (pwa_tensor(p, q, mηc^2 - q02^2; cl=cl) - a / 3 * pwa_contact(p, q, mηc^2 - q02^2; cl=cl)) * 2
+    res += (-C) * (√2 * g / fπ)^2 / 4 * pwa_tensor_ss(pon, qon, mηc^2 - q02^2; a=a) * 2
     return res
 end
 
-function potential_Xc(E, p, q; C=1, a=0, a1=0.0, isπ=true)
+function potential_Xc(E, p, q, pon, qon; C=1, a=0, a1=0.0, isπ=true)
     q01, q02 = q0f(E, mDc, mDsc, mDc, mDsc), q0f(E, mDc, mDsc, mDsc, mDc)
     res = zero(ComplexF64)
 
     if isπ == true
-        res += (-C) * (√2 * g / fπ)^2 / 4 * (pwa_tensor(p, q, mπn^2 - q02^2; cl=cl) - a / 3 * pwa_contact(p, q, mπn^2 - q02^2; cl=cl))
+        res += (-C) * (√2 * g / fπ)^2 / 4 * pwa_tensor_ss_deform(p, q, mπn^2 - q02^2; a=a1)
     end
-    res += (-C) * 1 / 3 * (√2 * g / fπ)^2 / 4 * (pwa_tensor(p, q, mη^2 - q02^2; cl=cl) - a / 3 * pwa_contact(p, q, mη^2 - q02^2; cl=cl))
-    res += -β^2 * gV^2 * (1 - q01^2 / mρn^2) / 4 * pwa_central(p, q, mρn^2 - q01^2; cl=cl)
-    res += ((-C) * (2 * gλ * gV)^2 / 4 * (pwa_tensor(p, q, mρn^2 - q02^2; cl=cl) + 2 * a / 3 * pwa_contact(p, q, mρn^2 - q02^2; cl=cl)
-                                          -
-                                          pwa_square(p, q, mρn^2 - q02^2; cl=cl)))
-    res += -β^2 * gV^2 * (1 - q01^2 / mω^2) / 4 * pwa_central(p, q, mω^2 - q01^2; cl=cl)
-    res += ((-C) * (2 * gλ * gV)^2 / 4 * (pwa_tensor(p, q, mω^2 - q02^2; cl=cl) + 2 * a / 3 * pwa_contact(p, q, mω^2 - q02^2; cl=cl)
-                                          -
-                                          pwa_square(p, q, mω^2 - q02^2; cl=cl)))
-    res += -4 * gs^2 / 4 * pwa_central(p, q, mσ^2 - q01^2; cl=cl)
+    res += (-C) * 1 / 3 * (√2 * g / fπ)^2 / 4 * pwa_tensor_ss(pon, qon, mη^2 - q02^2; a=a)
+    res += -β^2 * gV^2 * (1 - q01^2 / mρn^2) / 4 * pwa_central(p, q, mρn^2 - q01^2)
+    res += (-C) * (2 * gλ * gV)^2 / 4 * pwa_vvv_ss(pon, qon, mρn^2 - q02^2; a=a)
+    res += -β^2 * gV^2 * (1 - q01^2 / mω^2) / 4 * pwa_central(p, q, mω^2 - q01^2)
+    res += (-C) * (2 * gλ * gV)^2 / 4 * pwa_vvv_ss(pon, qon, mω^2 - q02^2; a=a)
+    res += -4 * gs^2 / 4 * pwa_central(p, q, mσ^2 - q01^2)
     #J/ψ
-    res += -β^2 * gV^2 * (1 - q01^2 / mJΨ^2) / 4 * pwa_central(p, q, mJΨ^2 - q01^2; cl=cl) * 2
-    res += ((-C) * (2 * gλ * gV)^2 / 4 * (pwa_tensor(p, q, mJΨ^2 - q02^2; cl=cl) + 2 * a / 3 * pwa_contact(p, q, mJΨ^2 - q02^2; cl=cl)
-                                          -
-                                          pwa_square(p, q, mJΨ^2 - q02^2; cl=cl)) * 2)
+    res += -β^2 * gV^2 * (1 - q01^2 / mJψ^2) / 4 * pwa_central(p, q, mJψ^2 - q01^2) * 2
+    res += (-C) * (2 * gλ * gV)^2 / 4 * pwa_vvv_ss(pon, qon, mJψ^2 - q02^2; a=a) * 2
     #ηc
-    res += (-C) * (√2 * g / fπ)^2 / 4 * (pwa_tensor(p, q, mηc^2 - q02^2; cl=cl) - a / 3 * pwa_contact(p, q, mηc^2 - q02^2; cl=cl)) * 2
+    res += (-C) * (√2 * g / fπ)^2 / 4 * pwa_tensor_ss(pon, qon, mηc^2 - q02^2; a=a) * 2
 
     return res
 end
 
-function potential_Xcross(E, p, q; C=1, a=0, a1=0.0, isπ=true)
+function potential_Xcross(E, p, q, pon, qon; C=1, a=0, a1=0.0, isπ=true)
     q01, q02 = q0f(E, mDn, mDsn, mDc, mDsc), q0f(E, mDn, mDsn, mDsc, mDc)
     res = zero(ComplexF64)
 
-    res += -(√2 * gV * β)^2 * (1 - q01^2 / mρc^2) / 4 * pwa_central(p, q, mρc^2 - q01^2; cl=cl)
-    res += ((-C) * (2 * √2 * gλ * gV)^2 / 4 * (pwa_tensor(p, q, mρc^2 - q02^2; cl=cl) + 2 * a / 3 * pwa_contact(p, q, mρc^2 - q02^2; cl=cl)
-                                               -
-                                               pwa_square(p, q, mρc^2 - q02^2; cl=cl)))
+    res += -(√2 * gV * β)^2 * (1 - q01^2 / mρc^2) / 4 * pwa_central(p, q, mρc^2 - q01^2)
+    res += (-C) * (2 * √2 * gλ * gV)^2 / 4 * pwa_vvv_ss(pon, qon, mρc^2 - q02^2; a=a)
     if isπ == true
-        res += (-C) * (2 * g / fπ)^2 / 4 * (pwa_tensor(p, q, mπc^2 - q02^2; cl=cl) - a / 3 * pwa_contact(p, q, mπc^2 - q02^2; cl=cl))
+        res += (-C) * (2 * g / fπ)^2 / 4 * pwa_tensor_ss(p, q, mπc^2 - q02^2; a=a1)
     end
     return res
 end
--- LSE/libs/pwa.jl
+++ LSE/libs/pwa.jl
@@ -3,30 +3,41 @@ function pwa_contact(p, q, usq)::ComplexF64
     return 1.0
 end
 
-# function pwa_central(p, q, usq)::ComplexF64
-#     ξ = (p^2 + q^2 + usq) / (2 * p * q)
-#     tmp = zero(ComplexF64)
-#     if iszero(p) || iszero(q)
-#         tmp = 1 / (p^2 + q^2 + usq)
-#     else
-#         tmp = -1 / (4 * p * q) * (log(ξ - 1) - log(ξ + 1))
-#     end
-#     return tmp
-# end
-
 function pwa_central(p, q, usq)::ComplexF64
     ξ = (p^2 + q^2 + usq) / (2 * p * q)
     tmp = zero(ComplexF64)
     if iszero(p) || iszero(q)
         tmp = 1 / (p^2 + q^2 + usq)
     else
-        tmp = -1 / (4 * p * q) * log(((p-q)^2+usq)/((p+q)^2+usq))
+        tmp = -1 / (4 * p * q) * log(((p - q)^2 + usq) / ((p + q)^2 + usq))
+    end
+    return tmp
+end
+
+function pwa_square(p, q, usq)::ComplexF64
+    ξ = (p^2 + q^2 + usq) / (2 * p * q)
+    tmp = zero(ComplexF64)
+    if iszero(p) || iszero(q)
+        tmp = (p^2 + q^2) / (p^2 + q^2 + usq)
+    else
+        tmp = -1 / (4 * p * q) * (-4 * p * q + (p^2 + q^2 - 2 * p * q * ξ) * log(((p - q)^2 + usq) / ((p + q)^2 + usq)))
     end
     return tmp
 end
 
 function pwa_tensor_ss(p, q, usq; a=0.0)::ComplexF64
-    return 1 / 3 * (1 - a) * pwa_contact(p, q, usq) - usq / 3 * pwa_central(p, q, usq)
+    ξ = (p^2 + q^2 + usq) / (2 * p * q)
+    tmp = zero(ComplexF64)
+    if iszero(p) || iszero(q)
+        tmp = 1 / 3 * (p^2 + q^2) / (p^2 + q^2 + usq)
+    else
+        tmp = -1 / (12 * p * q) * (-4 * p * q + (p^2 + q^2 - 2 * p * q * ξ) * log(((p - q)^2 + usq) / ((p + q)^2 + usq)))
+    end
+    return tmp - a / 3 * pwa_contact(p, q, usq)
+end
+
+function pwa_vvv_ss(p, q, usq; a=0.0)::ComplexF64
+    return pwa_tensor_ss(p, q, usq; a=a) - pwa_square(p, q, usq)
 end
 
 
@@ -49,10 +60,10 @@ function pwa_central_deform(p, q, usq)
     if iszero(p) || iszero(q)
         tmp = 1 / (p^2 + q^2 + usq)
     else
-        if imag(ξ)<0&&abs(real(ξ))<1
-            tmp=-1 / (4 * p * q) * (log(((p-q)^2+usq)/((p+q)^2+usq))+im*2*π)
+        if imag(ξ) < 0 && abs(real(ξ)) < 1
+            tmp = -1 / (4 * p * q) * (log(((p - q)^2 + usq) / ((p + q)^2 + usq)) + im * 2 * π)
         else
-            tmp=-1 / (4 * p * q) * log(((p-q)^2+usq)/((p+q)^2+usq))
+            tmp = -1 / (4 * p * q) * log(((p - q)^2 + usq) / ((p + q)^2 + usq))
         end
     end
     return tmp
